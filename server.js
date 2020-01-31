c7980724be3bb128ceafbee7d618a4fd6bbd78bb
ferb00000100 <fhlh@protonmail.com>
2020-01-31 09:23:00 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(require("./routes/apiRoutes"));
+app.use(require("./routes/htmlRoutes"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+	useNewUrlParser: true
+});
+
+app.listen(PORT, function() {
+	console.log(`Now listening on port: ${PORT}`);
+});
+
